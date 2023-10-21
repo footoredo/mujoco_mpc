4b6dba4816365bf8da9ca55d9a96595b29e909b0
JaLnYn <alan.yuan.jly@gmail.com>
2023-10-21 14:36:31 -0400

    added quadruped climber

--- mjpc/tasks/quadruped/quadruped.h
+++ mjpc/tasks/quadruped/quadruped.h
@@ -285,6 +285,44 @@ class QuadrupedHill : public ThreadSafeTask {
   ResidualFn residual_;
 };
 
+class QuadrupedClimber : public ThreadSafeTask {
+ public:
+  std::string Name() const override;
+  std::string XmlPath() const override;
+  class ResidualFn : public mjpc::BaseResidualFn {
+   public:
+    explicit ResidualFn(const QuadrupedClimber* task, int current_mode = 0)
+        : mjpc::BaseResidualFn(task), current_mode_(current_mode) {}
+
+    // --------------------- Residuals for quadruped task --------------------
+    //   Number of residuals: 4
+    //     Residual (0): position_z - average(foot position)_z - height_goal
+    //     Residual (1): position - goal_position
+    //     Residual (2): orientation - goal_orientation
+    //     Residual (3): control
+    //   Number of parameters: 1
+    //     Parameter (1): height_goal
+    // -----------------------------------------------------------------------
+    void Residual(const mjModel* model, const mjData* data,
+                  double* residual) const override;
+   private:
+    friend class QuadrupedClimber;
+    int current_mode_;
+  };
+  QuadrupedClimber() : residual_(this) {}
+  void TransitionLocked(mjModel* model, mjData* data) override;
+
+ protected:
+  std::unique_ptr<mjpc::ResidualFn> ResidualLocked() const override {
+    return std::make_unique<ResidualFn>(this, residual_.current_mode_);
+  }
+  ResidualFn* InternalResidual() override { return &residual_; }
+
+ private:
+  ResidualFn residual_;
+};
+
+
 }  // namespace mjpc
 
 #endif  // MJPC_TASKS_QUADRUPED_QUADRUPED_H_
--- mjpc/tasks/tasks.cc
+++ mjpc/tasks/tasks.cc
@@ -53,6 +53,7 @@ std::vector<std::shared_ptr<Task>> GetTasks() {
     std::make_shared<Quadrotor>(),
     std::make_shared<QuadrupedFlat>(),
     std::make_shared<QuadrupedHill>(),
+    std::make_shared<QuadrupedClimber>(),
     std::make_shared<Swimmer>(),
     std::make_shared<Walker>(),
   };
